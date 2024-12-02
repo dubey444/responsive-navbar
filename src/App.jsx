db060b73b599bc9a63a664447d7554fae337e64f
dubey444 <mr.dubey44@gmail.com>
2024-12-02 00:56:29 -0800

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import Navbar from "./Components/Navbar";
+
+function App() {
+	return (
+		<Navbar/>
+	);
+}
+
+export default App;
